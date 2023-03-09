61efb21605f41670eaa6c1091ff50b9c1c7c573b
nikhilvishwa7 <123230101+nikhilvishwa7@users.noreply.github.com>
2023-03-09 22:51:08 +0530

    Update sample_info.py

--- sample_info.py
+++ sample_info.py
@@ -1,24 +1,24 @@
 # Bot information
 SESSION = 'LuciferMoringstar_Robot'
 USER_SESSION = 'User_Bot'
-API_ID = 12345
-API_HASH = '0123456789abcdef0123456789abcdef'
-BOT_TOKEN = '123456:LuciferMoringstar_Robot-zyx57W2v1u123ew11'
+API_ID = 23640300
+API_HASH = 'b43066e201bfc457ad566e623fd74dab'
+BOT_TOKEN = :'6074635868:AAHrgH0A5Vd8tG0HlWnUTT2o8wM6ym4OtAw'
 USERBOT_STRING_SESSION = 'LuciferMoringstar_Robot'
 
 # Bot settings
 CACHE_TIME = 300
-USE_CAPTION_FILTER = False
+USE_CAPTION_FILTER = True
 
 # Admins, Channels & Users
-ADMINS = [12345789, 'admin123', 987654321]
+ADMINS = ['5149183428']
 CHANNELS = [-10012345678, -100987654321, 'Mo_Tech_YT']
-AUTH_USERS = []
+AUTH_USERS = ['5149183428']
 AUTH_CHANNEL = None
 
 # MongoDB information
-DATABASE_URI = "mongodb://[LuciferMoringstar_Robot:LuciferMoringstar_Robot@]host1[:port1][,...hostN[:portN]][/[defaultauthdb]?retryWrites=true&w=majority"
-DATABASE_NAME = 'LuciferMoringstar_Robot'
+DATABASE_URI = "mongodb+srv://Nikhil_Vishwa7:nikhil_vishwa7@cluster0.dla9mvs.mongodb.net/?retryWrites=true&w=majority"
+DATABASE_NAME = 'ProjectNV'
 COLLECTION_NAME = 'channel_files'  # If you are using the same database, then use different collection name for each bot
 
 # Messages
